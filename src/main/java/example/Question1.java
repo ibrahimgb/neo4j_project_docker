60fcea407f4c1e3cc3f213efb8e6d1348dbdd142
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-07-19 18:23:02 +0200

    not working script

--- src/main/java/example/Question1.java
+++ src/main/java/example/Question1.java
@@ -1,7 +1,6 @@
 package example;
 
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Mode;
@@ -11,22 +10,28 @@ import java.util.stream.Stream;
 
 /**
  * This is an example returning {@link org.neo4j.graphdb.Entity Entities} from stored procedures.
- * {@link Node Nodes} and {@link org.neo4j.graphdb.Relationship relationships} are both entities
+ * {@link org.neo4j.graphdb.Node Nodes} and {@link org.neo4j.graphdb.Relationship relationships} are both entities
  * and can only be accessed in their transaction. So it is important that you use the injected one
  * and not open a new one; otherwise you can access them from the outside.
  */
 public class Question1 {
 
-	@Context
-	public Transaction tx;
+    @Context
+    public Transaction tx;
 
-	public record EntityContainer(String title,int reviews) {
-	}
+    public record EntityContainer(String title, int reviews) {
+    }
 
-	@Procedure(name = "recommend.howManyReview", mode = Mode.READ)
-	public Stream<EntityContainer> howManyReview( /** TODO fixme**/) {
-		//TODO fixme
-		//ResourceIterator<Node> nodes = tx.execute("MATCH (n) RETURN n").columnAs("n");
-		return Stream.empty(); //TODO fixme
-	}
+    @Procedure(name = "recommend.howManyReview", mode = Mode.READ)
+    public Stream<EntityContainer> howManyReview() {
+        String apocQuery = "MATCH (m:Movie)<-[:RATED]-(u:User)\n" +
+                "WHERE toLower(m.title) CONTAINS 'matrix'\n" +
+                "WITH m, count(*) AS reviews\n" +
+                "RETURN m.title AS movie, reviews\n" +
+                "ORDER BY reviews DESC LIMIT 5;";
+
+        Result result = tx.execute(apocQuery);
+        return result.stream()
+                .map(row -> new EntityContainer((String) row.get("movie"), ((Long) row.get("reviews")).intValue()));
+    }
 }

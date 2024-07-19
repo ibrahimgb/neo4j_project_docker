62253f199e073e5d1ca060bdead61e90e1a22798
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-07-19 18:37:25 +0200

    not working ?

--- src/main/java/example/Question1.java
+++ src/main/java/example/Question1.java
@@ -6,32 +6,38 @@ import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Mode;
 import org.neo4j.procedure.Procedure;
 
+import java.util.Map;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-/**
- * This is an example returning {@link org.neo4j.graphdb.Entity Entities} from stored procedures.
- * {@link org.neo4j.graphdb.Node Nodes} and {@link org.neo4j.graphdb.Relationship relationships} are both entities
- * and can only be accessed in their transaction. So it is important that you use the injected one
- * and not open a new one; otherwise you can access them from the outside.
- */
 public class Question1 {
 
     @Context
     public Transaction tx;
 
-    public record EntityContainer(String title, int reviews) {
+
+    public static class EntityContainer {
+        public String title;
+        public int reviews;
+
+        public EntityContainer(String title, int reviews) {
+            this.title = title;
+            this.reviews = reviews;
+        }
     }
 
     @Procedure(name = "recommend.howManyReview", mode = Mode.READ)
     public Stream<EntityContainer> howManyReview() {
-        String apocQuery = "MATCH (m:Movie)<-[:RATED]-(u:User)\n" +
-                "WHERE toLower(m.title) CONTAINS 'matrix'\n" +
-                "WITH m, count(*) AS reviews\n" +
-                "RETURN m.title AS movie, reviews\n" +
-                "ORDER BY reviews DESC LIMIT 5;";
+        String apocQuery = "MATCH (m:Movie)<-[:RATED]-(u:User) " +
+                "WHERE toLower(m.title) CONTAINS 'matrix' " +
+                "WITH m, count(*) AS reviews " +
+                "RETURN m.title AS movie, reviews " +
+                "ORDER BY reviews DESC LIMIT 5";
 
-        Result result = tx.execute(apocQuery);
-        return result.stream()
-                .map(row -> new EntityContainer((String) row.get("movie"), ((Long) row.get("reviews")).intValue()));
+            Result result = tx.execute(apocQuery);
+            Iterable<Map<String, Object>> iterable = () -> result;
+            return StreamSupport.stream(iterable.spliterator(), false)
+                    .map(row -> new EntityContainer((String) row.get("movie"), ((Long) row.get("reviews")).intValue()));
+
     }
 }
